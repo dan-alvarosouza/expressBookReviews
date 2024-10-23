a388dce6c486f940a6c1b61aa6a04fee93e78ccb
dan-alvarosouza <dan.alvarosouza@gmail.com>
2024-10-23 08:49:16 -0400

    index.js updated

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if there's a JWT in the request header
+    console.log(req.session)
+        if (req.session) {
+            if (req.session.authorization) {
+                let token = req.session.authorization.token;
+                jwt.verify(token, 'my-secret-key', (err, user) => {
+                    if (err) {
+                        //Token verification failed
+                        res.status(401).json({ error: 'galek err' });
+                    } else {
+                        req.session.user = user;
+                        next();
+                    }
+                });
+            } else {
+                res.status(401).json({ error: 'Unauthorized here' });
+            }
+        }
+    });
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
